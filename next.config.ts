d2f0135fc8260c00c5e7e7cf2e50d111da6f66f8
yuyuyu0929 <yun078728@gmail.com>
2026-03-18 21:16:15 +0900

    fix: CSPヘッダーを無効化

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://*.supabase.co https://rikkeyapp.net;",
+            value: "",
           },
         ],
       },
